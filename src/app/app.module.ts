fa8e36f1d42a0f2afe6040915f8d71b9c00bad21
Mikkel Frederiksen <5760459@rts-365.dk>
2021-06-16 18:35:12 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation/navigation.component';
+import { LogoComponent } from './navigation/logo/logo.component';
+import { SigninBtnComponent } from './navigation/signin-btn/signin-btn.component';
+import { NavigationListComponent } from './navigation/navigation-list/navigation-list.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LogoComponent,
+    SigninBtnComponent,
+    NavigationListComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/signin-btn/signin-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navigation-signin-btn',
+  templateUrl: './signin-btn.component.html',
+  styleUrls: ['./signin-btn.component.scss']
+})
+export class SigninBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
